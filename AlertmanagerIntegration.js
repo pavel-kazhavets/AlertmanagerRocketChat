93d83f2b7c93baf00cd907b6ec77f0600251a4ac
Vitaly <v-zhuravlev@users.noreply.github.com>
2023-04-11 18:02:46 +0800

    - Collapse alerts with index=2 or more - Print commonAlerts/commonAnnotations - Add silence_url link (works with grafanas alertmanager) - Add grafana_url link - Add runbook_url link

--- AlertmanagerIntegration.js
+++ AlertmanagerIntegration.js
@@ -1,25 +1,23 @@
+// https://developer.rocket.chat/reference/api/rest-api/endpoints/core-endpoints/chat-endpoints/postmessage#message-object-example
+// https://prometheus.io/docs/alerting/latest/notifications/
 class Script {
     process_incoming_request({
         request
     }) {
-        console.log(request.content);
-
-        var alertColor = "warning";
-        if (request.content.status == "resolved") {
-            alertColor = "green";
-        } else if (request.content.status == "firing") {
-            alertColor = "danger";
-        }
-
-        let finFields = [];
+        //console.log(request.content);
+        
+        var commonText = "";
+        let alerts = [];
+        if (!!request.content.groupLabels) {
+            commonText = this.getCommonText(request.content);
+        };
         for (i = 0; i < request.content.alerts.length; i++) {
+            let finFields = [];
+            // default color
+            var alertColor = "red";
             var endVal = request.content.alerts[i];
-            var elem = {
-                title: "Alertname",
-                value: endVal.labels.alertname,
-                short: false
-            };
-            finFields.push(elem);
+            var title = endVal.labels.alertname;
+
 
             if (!!endVal.annotations.summary) {
                 finFields.push({
@@ -27,13 +25,20 @@ class Script {
                     value: endVal.annotations.summary
                 });
             }
+            
 
-            if (!!endVal.labels.severity) {
-                finFields.push({
-                    title: "Severity",
-                    value: endVal.labels.severity
-                });
+            // set color 
+            if (endVal.status == "resolved") {
+                alertColor = "green";
+            } else if (!!endVal.labels.severity && endVal.labels.severity == "warning") {
+                alertColor = "warning";
+            } else if (!!endVal.labels.severity && endVal.labels.severity == "info") {
+                alertColor = "green";
+            } else if (!!endVal.labels.severity && endVal.labels.severity == "none") {
+                alertColor = "grey";
             }
+            
+
 
             if (!!endVal.annotations.description) {
                 finFields.push({
@@ -49,24 +54,47 @@ class Script {
                 });
             }
 
+            var links = [];
+            links = this.getActionButtons(endVal);
+            if (links.length > 0) {
+                finFields.push({
+                    title: "Links",
+                    value: links.join(" ")
+                });
+            }
+
             finFields.push({
                 title: "Labels",
-                value: this.getLabelsField(endVal.labels)
+                value: this.getLabelsField(endVal.labels),
+                short: false,
             });
+            
+
+            //collapse all alerts except first two:
+            var collapsed = false;
+            if (i > 1) {
+                collapsed = true;
+            }
+            alerts.push(
+                {
+                    color: alertColor,
+                    title_link: request.content.externalURL,
+                    title: endVal.status.toUpperCase()+": "+title,
+                    fields: finFields,
+                    collapsed: collapsed
+                }
+            )
         }
 
         return {
             content: {
                 username: "Prometheus Alert",
-                attachments: [{
-                    color: alertColor,
-                    title_link: request.content.externalURL,
-                    title: "Prometheus notification",
-                    fields: finFields
-                }]
+                text: commonText,
+                attachments: alerts,
             }
         };
     }
+
 
     getLabelsField(labelsObj) {
         let labelsArr = [];
@@ -74,9 +102,69 @@ class Script {
             if (key == "alertname" || key == "severity") {
                 continue;
             }
-            labelsArr.push(key+"="+labelsObj[key]);
+            labelsArr.push("`"+key+"="+labelsObj[key]+"`");
         }
-
         return labelsArr.join(", ");
+    }
+    getLabelsURL(labelsObj) {
+        let labelsArr = [];
+        for (const key of Object.keys(labelsObj)) {
+            labelsArr.push(key+"%3D"+labelsObj[key]);
+        }
+        return labelsArr.join("&matcher=");
+    }
+
+    getActionButtons(endVal) {
+        let actions = [];
+
+        if (!!endVal.annotations.silence_url && endVal.status == "firing") {
+            actions.push("[🔕Silence]("+endVal.annotations.silence_url+"&"+this.getLabelsURL(endVal.labels)+")");
+        }
+        if (!!endVal.annotations.dashboard_url) {
+            actions.push("[📈Dashboard]("+endVal.annotations.dashboard_url+")");
+        }
+        if (!!endVal.annotations.runbook_url) {
+            actions.push("[📖Runbook]("+endVal.annotations.runbook_url+")");
+        }
+        return actions;
+    }
+
+    getCommonText(content) {
+        let commonText = [];
+        let groupLabels = [];
+        let commonLabels = [];
+        let commonAnnotations = [];
+        if (!!content.groupLabels) {
+            for (const key of Object.keys(content.groupLabels)) {
+                if (key == "alertname" || key == "severity") {
+                    continue;
+                }
+                groupLabels.push("`"+key+"="+content.groupLabels[key]+"`");
+            }
+            commonText.push("Alerts are grouped by: "+groupLabels.join(", "));
+        };
+
+        if (!!content.commonLabels) {
+            for (const key of Object.keys(content.commonLabels)) {
+                if (key == "alertname" || key == "severity") {
+                    continue;
+                }
+                commonLabels.push("`"+key+"="+content.commonLabels[key]+"`");
+            }
+            commonText.push("Common labels: " + commonLabels.join(", "));
+        };
+
+        if (!!content.commonAnnotations) {
+            for (const key of Object.keys(content.commonAnnotations)) {
+                if (key == "alertname" || key == "severity") {
+                    continue;
+                }
+                commonAnnotations.push("**"+key+"**: "+content.commonAnnotations[key]+"\n");
+            }
+            commonText.push(commonAnnotations.join(", "));
+        };
+
+        return commonText.join("\n");
+
     }
 }
